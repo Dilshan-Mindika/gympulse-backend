e36b5d9596210e9778c88ad37cbbfd7faa396d60
Dilshan Mindika <84672676+Dilshan-Mindika@users.noreply.github.com>
2024-11-21 12:03:16 +0530

    initial commit

---
+++ src/main/java/com/gympulse/gympulse/GympulseApplication.java
@@ -0,0 +1,13 @@
+package com.gympulse.gympulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GympulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GympulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gympulse/gympulse/GympulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gympulse.gympulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GympulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
