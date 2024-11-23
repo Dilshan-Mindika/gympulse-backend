767bdb2e06e3ec354bb05e959e2695ce3c105f75
uvindupasan <uvindupasankumarasinghe@gmail.com>
2024-11-23 17:00:35 +0530

    Add MemberServiceImpl for member creation and logging

--- src/main/java/com/gympulse/gympulse/repositories/MemberRepository.java
+++ src/main/java/com/gympulse/gympulse/repositories/MemberRepository.java
@@ -1,8 +1,4 @@
 package com.gympulse.gympulse.repositories;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MemberRepository  extends MongoRepository<Member,ObjectId> {
-    // finds a menber by thier unique member ID
-    Optional<Member> findByMemberId(String memberId);
+public interface MemberRepository {
 }
---
+++ src/main/java/com/gympulse/gympulse/service/Implimentation/MemberServiceImpl.java
@@ -0,0 +1,32 @@
+package com.gympulse.gympulse.service.Implimentation;
+
+
+import com.gympulse.gympulse.model.person.Member;
+import com.gympulse.gympulse.repositories.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+    private GymLogger logger = GymLogger.getInstance();
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+
+    @Override
+    public Member createMember(String fullName, String email, String address, String phoneNumber,
+                               String memberShipType, String startdate, String endDate,WorkoutPlan workoutPlan) {
+
+        //Generate a unique member ID
+        String memberId = generateNextMemberId();
+        if (memberId =! null ) {
+            //Create and save the new Member
+            Member menber = memberRepository.insert(new Member(fullName, email, address, phoneNumber,memberId, memberShipType, startDate,endDate, workoutPlan));
+            logger.log("New Member added, Member ID :" + memberId);
+            return member;
+        } else {
+            throw new IllegalStateException("Member limit reached. Connot creat more members.")
+        }
+    }
+}
