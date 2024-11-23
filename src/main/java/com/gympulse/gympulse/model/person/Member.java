6e967b01c4314ed0763eeede6e80fba8ccf8a4fd
uvindupasan <uvindupasankumarasinghe@gmail.com>
2024-11-23 14:44:43 +0530

    Constructor for creating a member with a workout plan

--- src/main/java/com/gympulse/gympulse/model/person/Member.java
+++ src/main/java/com/gympulse/gympulse/model/person/Member.java
@@ -10,5 +10,18 @@ public class Member {
     @DocumentReference //reference to workplan document
     private WorkoutPlan workoutPlan; // associated workout plan for the member
 
+    //Constructor for creating a member with a workout plan
+    public Member (String fullName,String email, String address, String phoneNumber, String memberId,
+                   String memberShipType, String startString, String endString ,WorkoutPlan workoutPlan ) {
+        super(fullName, email, address, phoneNumber); // Initialize fields from the Person superclass
+        this.memberId = memberId; //initialize member-specific fields
+        this.memberShipType = memberShipType;
+        this.startDate = startString;
+        this.workoutPlan = workoutPlan; // set the workout plan
+
+    }
+
+
+
 
 }
