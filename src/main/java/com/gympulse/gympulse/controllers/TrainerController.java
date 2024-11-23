628226a15a123f4b897ea7834a040161615cacaf
Ruukshan <rukshaninduwara123@gmail.com>
2024-11-23 19:26:00 +0530

    Set up Create user method in controller class

--- src/main/java/com/gympulse/gympulse/controllers/TrainerController.java
+++ src/main/java/com/gympulse/gympulse/controllers/TrainerController.java
@@ -1,13 +1,12 @@
 package com.gympulse.gympulse.controllers;
 
 import com.gympulse.gympulse.model.person.Trainer;
+import com.gympulse.gympulse.requests.TrainerRequest;
 import com.gympulse.gympulse.service.interfaces.TrainerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +20,19 @@ public class TrainerController {
     @GetMapping
     public ResponseEntity<List<Trainer>> getAllTrainers() {
         return new ResponseEntity<List<Trainer>>(trainerService.allTrainers(), HttpStatus.OK)
+    }
+
+    @PostMapping
+    public Trainer createTrainer(@RequestBody TrainerRequest trainerRequest) {
+        String speciality = trainerRequest.getSpeciality();
+        Double salary = trainerRequest.getSalary();
+        String certificationNumber = trainerRequest.getCertificationNumber();
+        String fullName = trainerRequest.getFullName();
+        String phoneNumber = trainerRequest.getPhoneNumber();
+        String address = trainerRequest.getAddress();
+        String email = trainerRequest.getEmail();
+
+        return trainerService.createTrainer(speciality, salary, certificationNumber, fullName,
+                phoneNumber, address, email);
     }
 }
