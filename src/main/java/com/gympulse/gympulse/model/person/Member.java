defc2aa52c45725170a83dafdb748bc523c9c727
uvindupasan <uvindupasankumarasinghe@gmail.com>
2024-11-23 15:51:19 +0530

    Constructor for creating a trainer without specifying trainer ID

--- src/main/java/com/gympulse/gympulse/model/person/Member.java
+++ src/main/java/com/gympulse/gympulse/model/person/Member.java
@@ -3,7 +3,7 @@ package com.gympulse.gympulse.model.person;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 public class Member {
-    private  String memberId; // unique identifier for the member
+    private String memberId; // unique identifier for the member
     private String memberShipType; // type of membership(standard,premium)
     private String startDate; //membership start date
     private String endDate; // Membership end date
@@ -11,17 +11,31 @@ public class Member {
     private WorkoutPlan workoutPlan; // associated workout plan for the member
 
     //Constructor for creating a member with a workout plan
-    public Member (String fullName,String email, String address, String phoneNumber, String memberId,
-                   String memberShipType, String startString, String endString ,WorkoutPlan workoutPlan ) {
+    public Member(String fullName, String email, String address, String phoneNumber, String memberId,
+                  String memberShipType, String startString, String endString, WorkoutPlan workoutPlan) {
         super(fullName, email, address, phoneNumber); // Initialize fields from the Person superclass
         this.memberId = memberId; //initialize member-specific fields
         this.memberShipType = memberShipType;
         this.startDate = startString;
         this.workoutPlan = workoutPlan; // set the workout plan
+    }
+    // Constructor for creating a trainer without specifying trainer ID (useful for new trainers)
+    public Member(String fullName, String email, String address, String phoneNumber, String memberId,
+                String membershipType, String startString, String endString) {
+            super(fullName, email, address, PhoneNumber);
+            this.memberId = memberId; //initialize member-specific fields
+            this.memberShipType = membershipType;
+            this.startDate = startString;
+            this.endDate = endString;
+            this.workoutPlan = null; // no workout plan assigned
 
     }
-
-
-
-
 }
+
+
+
+
+
+
+
+
