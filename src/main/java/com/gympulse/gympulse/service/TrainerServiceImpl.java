6c6773f06030c819088458118bd5c84c8571cebe
Ruukshan <rukshaninduwara123@gmail.com>
2024-11-23 17:51:29 +0530

    Update TrainerServiceImpl.java
    Create Trainer Implementation

--- src/main/java/com/gympulse/gympulse/service/TrainerServiceImpl.java
+++ src/main/java/com/gympulse/gympulse/service/TrainerServiceImpl.java
@@ -1,4 +1,23 @@
 package com.gympulse.gympulse.service;
 
-public class TrainerServiceImpl {
+import com.gympulse.gympulse.model.person.Trainer;
+import com.gympulse.gympulse.repositories.TrainerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TrainerServiceImpl implements TrainerService{
+
+    @Autowired
+    private TrainerRepository createTrainer(String trainerId, String speciality, Double salary, String certificationNumber,
+                                            String fullName, String phoneNumber, String address, String email){
+        // Generates a unique trainer ID
+        String trainerId = generateNextTrainerId();
+        if (trainerId != null) {
+            Trainer trainer = trainerRepository.insert(new Trainer(trainerId, speciality, salary, certificationNumber,
+                    fullName, phoneNumber, address, email));
+            return trainer;
+        } else {
+            throw new IllegalStateException("Trainer limit reached. Cannot create more trainers.");
+        }
+    }
+    
 }
