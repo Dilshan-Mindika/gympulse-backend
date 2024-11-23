612482d387d916b0663397f1e3e4038a88c29415
Ruukshan <rukshaninduwara123@gmail.com>
2024-11-23 18:41:46 +0530

    Set up GymLogger utill .
    Implements Logger and include it to the necessary methods

--- src/main/java/com/gympulse/gympulse/service/TrainerServiceImpl.java
+++ src/main/java/com/gympulse/gympulse/service/TrainerServiceImpl.java
@@ -2,7 +2,9 @@ package com.gympulse.gympulse.service;
 
 import com.gympulse.gympulse.model.person.Trainer;
 import com.gympulse.gympulse.repositories.TrainerRepository;
+import com.gympulse.gympulse.utils.GymLogger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -10,19 +12,23 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+@Service
 public class TrainerServiceImpl implements TrainerService{
+
+    private GymLogger logger = GymLogger.getInstance();
 
     @Autowired
     private TrainerRepository trainerRepository;
 
     @Override
-    public Trainer createTrainer(String trainerId, String speciality, Double salary, String certificationNumber,
+    public Trainer createTrainer(String speciality, Double salary, String certificationNumber,
                                             String fullName, String phoneNumber, String address, String email){
         // Generates a unique trainer ID
         String trainerId = generateNextTrainerId();
         if (trainerId != null) {
             Trainer trainer = trainerRepository.insert(new Trainer(trainerId, speciality, salary, certificationNumber,
                     fullName, phoneNumber, address, email));
+            logger.log("New Trainer created, Trainer ID: " + trainerId);
             return trainer;
         } else {
             throw new IllegalStateException("Trainer limit reached. Cannot create more trainers.");
@@ -31,7 +37,7 @@ public class TrainerServiceImpl implements TrainerService{
 
     @Override
     public List<Trainer> allTrainers(){
-        return trainerRepository.findAll()
+        return trainerRepository.findAll();
     }
 
     @Override
@@ -51,6 +57,7 @@ public class TrainerServiceImpl implements TrainerService{
             trainer.setPhoneNumber(trainerRequest.getPhoneNumber());
             trainer.setAddress(trainerRequest.getAddress());
             trainer.setEmail(trainerRequest.getEmail());
+            logger.log("Trainer updated, Trainer ID: " + trainerId);
             return trainerRepository.save(trainer);
         } else {
             throw new NoSuchElementException("Trainer not found for ID: " + trainerId);
@@ -61,6 +68,7 @@ public class TrainerServiceImpl implements TrainerService{
     public void deleteByTrainerId(String trainerId) {
         Optional<Trainer> trainer = trainerRepository.findByTrainerId(trainerId);
         if (trainer.isPresent()) {
+            logger.log("Trainer deleted, ID: " + trainerId);
             trainerRepository.delete(trainer.get());
         } else {
             throw new NoSuchElementException("Trainer not found for ID: " + trainerId);
