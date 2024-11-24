c5faf90a47a9f3c54d8431e8815a46c328d8d95d
uvindupasan <uvindupasankumarasinghe@gmail.com>
2024-11-24 11:38:19 +0530

    create repository interface

--- src/main/java/com/gympulse/gympulse/repositories/MemberRepository.java
+++ src/main/java/com/gympulse/gympulse/repositories/MemberRepository.java
@@ -1,4 +1,11 @@
 package com.gympulse.gympulse.repositories;
 
-public interface MemberRepository {
+import com.gympulse.gympulse.model.person.Member;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends MongoRepository <Member , ObjectId> {
+    //find a member by their unique member ID
+    Optional<Member> find ByMemberId(String memberID);
 }
