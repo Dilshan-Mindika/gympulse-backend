2db47b9b683dbe18e0ce42d7b7f59d60533e540c
kavindyasandeepani <cb8b11171@kiu.ac.lk>
2024-11-24 15:38:41 +0530

    add postmapping

--- src/main/java/Controller/EquipmentController.java
+++ src/main/java/Controller/EquipmentController.java
@@ -8,3 +8,15 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin(origins ="*")
 @RequestMapping("api/v1/equipment")
+
+public class EquipmentController {
+
+    @Autowired
+    private EquipmentService equipmentService;
+
+    @PostMapping(value = "/save")
+    private String saveEquipment(@RequestBody Equipment equipments)
+    {
+        equipmentService.saveorUpdate(equipments);
+        return equipments.get_id();
+    }
