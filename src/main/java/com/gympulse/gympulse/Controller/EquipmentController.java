1c063f9e1bad2fe5da92a3164c6dc3061929eceb
kavindyasandeepani <cb8b11171@kiu.ac.lk>
2024-11-24 20:57:59 +0530

    edit annotation

--- src/main/java/com/gympulse/gympulse/Controller/EquipmentController.java
+++ src/main/java/com/gympulse/gympulse/Controller/EquipmentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins ="*")
-@RequestMapping("api/v1/equipment")?
+@RequestMapping("api/v1/equipment")
 
 
 public class EquipmentController {
