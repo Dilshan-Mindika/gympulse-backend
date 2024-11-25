c22001946b176e6408d625813d19ba0f0fd7087f
Dilshan Mindika <84672676+Dilshan-Mindika@users.noreply.github.com>
2024-11-25 16:13:41 +0530

    Update Trainer.java

--- src/main/java/com/gympulse/gympulse/model/person/Trainer.java
+++ src/main/java/com/gympulse/gympulse/model/person/Trainer.java
@@ -1,4 +1,4 @@
-package com.nexus.GYMPULSE.model.person;
+package com.gympulse.gympulse.model.person;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
