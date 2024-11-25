7411845ea5c525a37951aedc572e438571c5e3b7
Dilshan Mindika <84672676+Dilshan-Mindika@users.noreply.github.com>
2024-11-25 16:20:19 +0530

    Update TrainerRepository.java

--- src/main/java/com/gympulse/gympulse/repositories/TrainerRepository.java
+++ src/main/java/com/gympulse/gympulse/repositories/TrainerRepository.java
@@ -1,4 +1,4 @@
-package com.nexus.GYMPULSE.repositories;
+package com.gympulse.gympulse.repositories;
 
 import java.util.Optional;
 
@@ -6,7 +6,7 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.nexus.GYMPULSE.model.person.Trainer;
+import com.gympulse.gympulse.model.person.Trainer;
 
 // Repository interface for managing Trainer entities in MongoDB
 @Repository
