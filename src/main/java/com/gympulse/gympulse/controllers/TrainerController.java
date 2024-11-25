8fd472898d6068e1ad4068d056ffedc7364c714f
Dilshan Mindika <84672676+Dilshan-Mindika@users.noreply.github.com>
2024-11-25 16:09:46 +0530

    Update TrainerController.java

--- src/main/java/com/gympulse/gympulse/controllers/TrainerController.java
+++ src/main/java/com/gympulse/gympulse/controllers/TrainerController.java
@@ -1,4 +1,4 @@
-package com.nexus.GYMPULSE.controllers;
+package com.gympulse.gympulse.controllers;
 
 import java.util.List;
 import java.util.Optional;
@@ -8,9 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.nexus.GYMPULSE.model.person.Trainer;
-import com.nexus.GYMPULSE.requests.TrainerRequest;
-import com.nexus.GYMPULSE.service.interfaces.TrainerService;
+import com.gympulse.gympulse.model.person.Trainer;
+import com.gympulse.gympulse.requests.TrainerRequest;
+import com.gympulse.gympulse.service.interfaces.TrainerService;
 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
