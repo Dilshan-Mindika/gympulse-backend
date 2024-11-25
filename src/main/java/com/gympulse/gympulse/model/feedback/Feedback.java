fa6f2fc63e41bbe3e249d0927ee7138bedc3580a
Dilshan Mindika <84672676+Dilshan-Mindika@users.noreply.github.com>
2024-11-25 16:11:49 +0530

    Update Feedback.java

--- src/main/java/com/gympulse/gympulse/model/feedback/Feedback.java
+++ src/main/java/com/gympulse/gympulse/model/feedback/Feedback.java
@@ -1,4 +1,4 @@
-package com.nexus.GYMPULSE.model.feedback;
+package com.gympulse.gympulse.model.feedback;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
