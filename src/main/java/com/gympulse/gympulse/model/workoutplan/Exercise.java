4c29c50322db760740f12d6d24958347f7ee9fbe
Dilshan Mindika <84672676+Dilshan-Mindika@users.noreply.github.com>
2024-11-25 16:15:59 +0530

    Update Exercise.java

--- src/main/java/com/gympulse/gympulse/model/workoutplan/Exercise.java
+++ src/main/java/com/gympulse/gympulse/model/workoutplan/Exercise.java
@@ -1,4 +1,4 @@
-package com.nexus.GYMPULSE.model.workoutplan;
+package com.gympulse.gympulse.model.workoutplan;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
